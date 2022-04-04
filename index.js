6ed43bbd9530a4bad312cd2667a9e2455ee5095a
Simba Wangulu <simbawangulu@gmail.com>
2022-04-04 16:58:15 +0300

    Update linter error

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 document.querySelector('.span_class').addEventListener('click', () => {
-  document.querySelector('.menu_item_container').classList.toggle('show')
-})
+  document.querySelector('.menu_item_container').classList.toggle('show');
+});
 
 document.querySelector('.close_icon').addEventListener('click', () => {
-  document.querySelector('.menu_item_container').classList.toggle('show')
-})
+  document.querySelector('.menu_item_container').classList.toggle('show');
+});
 
 document.querySelectorAll('.navlist-item-mobile').forEach((link) => {
   link.addEventListener('click', () => {
-    document.querySelector('.menu_item_container').classList.toggle('show')
-  })
-})
+    document.querySelector('.menu_item_container').classList.toggle('show');
+  });
+});
