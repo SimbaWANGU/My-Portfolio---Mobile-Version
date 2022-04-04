fa74aaf84f91c43d95301e23811784dd1d57b4dd
ichala <alijandhd@gmail.com>
2022-04-04 14:23:26 +0100

    Add Mobile Menu Functions

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ document.querySelector('.span_class').addEventListener('click', () => {
 document.querySelector('.close_icon').addEventListener('click', () => {
   document.querySelector('.menu_item_container').classList.toggle('show')
 })
+
+document.querySelectorAll('.navlist-item-mobile').forEach((link) => {
+  link.addEventListener('click', () => {
+    document.querySelector('.menu_item_container').classList.toggle('show')
+  })
+})
