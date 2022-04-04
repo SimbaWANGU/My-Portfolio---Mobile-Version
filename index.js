b85284521098f5bf1379382f88bc2797ede196e7
Simba Wangulu <simbawangulu@gmail.com>
2022-04-04 16:16:07 +0300

    Addmenu button and close menu functionality

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector('.span_class').addEventListener('click', () => {
+  document.querySelector('.menu_item_container').classList.toggle('show')
+})
+
+document.querySelector('.close_icon').addEventListener('click', () => {
+  document.querySelector('.menu_item_container').classList.toggle('show')
+})
