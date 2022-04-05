92d59cebe0f8a56eb651d15b6c707de22b133800
ichala <alijandhd@gmail.com>
2022-04-05 11:16:54 +0100

    Fix Blur Css

--- index.js
+++ index.js
@@ -1,38 +1,22 @@
-function displayMenu(){
+
   document.querySelector('.span_class').addEventListener('click', () => {
     document.querySelector('.menu_item_container').classList.toggle('show');
+    document.querySelectorAll('.mobile_background').forEach((section)=>{
+      section.classList.toggle('blur');
+    })
 });
-}
-
-function blurBackground(){
-  document.querySelector('.span_class').addEventListener('click', () => {
-    document.querySelector('.background').classList.toggle('blur');
+document.querySelector('.close_icon').addEventListener('click', () => {
+  document.querySelector('.menu_item_container').classList.toggle('show');
+  document.querySelectorAll('.mobile_background').forEach((section)=>{
+    section.classList.toggle('blur');
+  })
 });
-}
-function changeAppearance(){
-  displayMenu()
-  blurBackground()
-}
-
-function closeMenu(){
-  document.querySelector('.close_icon').addEventListener('click', () => {
-    document.querySelector('.menu_item_container').classList.toggle('show');
-  });
-}
-
-function unblurBackground(){
-  document.querySelector('.span_class').addEventListener('click', () => {
-    document.querySelector('.background').classList.toggle('blur');
-});
-}
-
-function returnAppearanceToNormal(){
-  closeMenu()
-  unblurBackground()
-}
 
 document.querySelectorAll('.navlist-item-mobile').forEach((link) => {
   link.addEventListener('click', () => {
     document.querySelector('.menu_item_container').classList.toggle('show');
+    document.querySelectorAll('.mobile_background').forEach((section)=>{
+      section.classList.toggle('blur');
+    })
   });
 });
