2ebaaf2238ab77865d25b79da13f90ef6bdb62a7
Simba Wangulu <simbawangulu@gmail.com>
2022-04-05 12:46:37 +0300

    Changes in html css and js files

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-document.querySelector('.span_class').addEventListener('click', () => {
-  document.querySelector('.menu_item_container').classList.toggle('show');
+function displayMenu(){
+  document.querySelector('.span_class').addEventListener('click', () => {
+    document.querySelector('.menu_item_container').classList.toggle('show');
 });
+}
 
-document.querySelector('.close_icon').addEventListener('click', () => {
-  document.querySelector('.menu_item_container').classList.toggle('show');
+function blurBackground(){
+  document.querySelector('.span_class').addEventListener('click', () => {
+    document.querySelector('.background').classList.toggle('blur');
 });
+}
+function changeAppearance(){
+  displayMenu()
+  blurBackground()
+}
+
+function closeMenu(){
+  document.querySelector('.close_icon').addEventListener('click', () => {
+    document.querySelector('.menu_item_container').classList.toggle('show');
+  });
+}
+
+function unblurBackground(){
+  document.querySelector('.span_class').addEventListener('click', () => {
+    document.querySelector('.background').classList.toggle('blur');
+});
+}
+
+function returnAppearanceToNormal(){
+  closeMenu()
+  unblurBackground()
+}
 
 document.querySelectorAll('.navlist-item-mobile').forEach((link) => {
   link.addEventListener('click', () => {
