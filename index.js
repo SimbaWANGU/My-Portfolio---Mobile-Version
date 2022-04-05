d8c6177c7e41866a00eb57074be5117c23d942f7
Simba Wangulu <simbawangulu@gmail.com>
2022-04-05 15:20:01 +0300

    Update linter errors

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 
-  document.querySelector('.span_class').addEventListener('click', () => {
-    document.querySelector('.menu_item_container').classList.toggle('show');
-    document.querySelectorAll('.mobile_background').forEach((section)=>{
-      section.classList.toggle('blur');
-    })
+document.querySelector('.span_class').addEventListener('click', () => {
+  document.querySelector('.menu_item_container').classList.toggle('show');
+  document.querySelectorAll('.mobile_background').forEach((section) => {
+  section.classList.toggle('blur');
+  });
 });
 document.querySelector('.close_icon').addEventListener('click', () => {
   document.querySelector('.menu_item_container').classList.toggle('show');
-  document.querySelectorAll('.mobile_background').forEach((section)=>{
+  document.querySelectorAll('.mobile_background').forEach((section) => {
     section.classList.toggle('blur');
-  })
+  });
 });
 
 document.querySelectorAll('.navlist-item-mobile').forEach((link) => {
   link.addEventListener('click', () => {
     document.querySelector('.menu_item_container').classList.toggle('show');
-    document.querySelectorAll('.mobile_background').forEach((section)=>{
+    document.querySelectorAll('.mobile_background').forEach((section) => {
       section.classList.toggle('blur');
-    })
+    });
   });
 });
 
-var scrollEventHandler = function()
-{
-  window.scroll(0, window.pageYOffset)
-}
+function scrollEventHandler(){
+  window.scroll(0, window.pageYOffset);
+};
 
-window.addEventListener("scroll", scrollEventHandler, false);
+window.addEventListener('scroll', scrollEventHandler, false);
